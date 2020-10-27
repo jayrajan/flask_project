6287c22d263537ab8b66750b64bb245193cb4e96
Jerin Rajan <jrajan@jprtech.co.uk>
2020-10-27 16:47:08 +0000

    feat: strings, slicing, dotformat, fstring literals

--- python/mypython.py
+++ python/mypython.py
@@ -1,1 +1,16 @@
-print('hello world')
+a = 'hello world'
+
+mystring = 'abcdefghijklmn'
+
+# slicing
+print(mystring[::-1])
+
+username = "Sammy"
+color = "blue"
+
+# dot format
+print("{} favoure color is {}".format(username,color))
+
+# f string literals!!!! 
+print(f"{username} chose {color}")
+
