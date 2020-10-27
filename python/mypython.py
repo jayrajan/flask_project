850811e5d0f41b61068f01e16ddae76fcb5e62d2
Jerin Rajan <jrajan@jprtech.co.uk>
2020-10-27 17:32:31 +0000

    feature: sets

--- python/mypython.py
+++ python/mypython.py
@@ -14,3 +14,14 @@ print("{} favoure color is {}".format(username,color))
 # f string literals!!!! 
 print(f"{username} chose {color}")
 
+# Sets - unique representation of unique elements
+x = set()
+x.add(1)
+x.add(2)
+x.add(3)
+x.add(3)
+x.add(3)
+print(x)
+
+myList = [1,2,3,3,3,4,4,4,1,2,33,44,2,2,2]
+print(set(myList))
