a608b2592d0e214dd8ff516dc38df29728721a81
Jerin Rajan <jrajan@jprtech.co.uk>
2020-10-28 17:31:07 +0000

    feat: indexing to capture value - hellos from dictionary

--- python/py_assessment_1.py
+++ python/py_assessment_1.py
@@ -52,7 +52,9 @@ d2 = {'k1':{'k2':'hello'}}
 
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 
-
+print(d1['simple_key'])
+print(d2['k1']['k2'])
+print(d3['k1'][0]['nest_key'][1][0])
 ###############
 ## Problem 4 ##
 ###############
