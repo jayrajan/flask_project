4398884f891ec82bd02200f3dd9465defdc1a758
Jerin Rajan <jrajan@jprtech.co.uk>
2020-10-28 17:23:49 +0000

    feat: reassigned list object

--- python/py_assessment_1.py
+++ python/py_assessment_1.py
@@ -37,7 +37,8 @@ print(s[::-1])
 # Given this nested list:
 mylist = [3,7,[1,4,'hello']]
 # Reassign "hello" to be "goodbye"
-
+mylist[2][2] = "goodbye"
+print(mylist)
 
 ###############
 ## Problem 3 ##
