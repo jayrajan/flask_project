e360ad1960eb7982fe9ba3d8cd87a26eed0199ad
Jerin Rajan <jrajan@jprtech.co.uk>
2020-10-29 17:49:13 +0000

    feat: convert to upper case, index first character of string, return the result

---
+++ python/first_upper.py
@@ -0,0 +1,11 @@
+# ## Task 3
+#
+# Create a function that takes in a string and returns back the
+# first character of that string in upper case.
+
+
+
+def first_upper(mystring):
+    return (mystring.upper()[0])
+
+print(first_upper('jay'))
