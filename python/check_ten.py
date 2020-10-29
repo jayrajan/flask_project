b3f9afef5a164ac94c7c06804a08b455d804f06c
Jerin Rajan <jrajan@jprtech.co.uk>
2020-10-29 17:38:03 +0000

    feat: sum of two int variables and logic to check their sum is 10

---
+++ python/check_ten.py
@@ -0,0 +1,9 @@
+# ## Task 1
+#
+#  Create a function that takes in two integers and returns
+## a Boolean True if their sum is 10, False if their sum is something else.
+
+def check_ten(n1,n2):
+    return( (n1+n2) == 10 )
+
+print(check_ten(10,0))
