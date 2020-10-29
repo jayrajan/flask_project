300ba32fec1df38e994a410194b49db1a20a2457
Jerin Rajan <jrajan@jprtech.co.uk>
2020-10-29 18:02:25 +0000

    feat: checking len of mystring, slicing the last two characters, returning error if len of string is less than 2 characters

---
+++ python/last_two.py
@@ -0,0 +1,17 @@
+# ## Task 4
+#
+# Create a function that takes in a string and returns the last two characters.
+# If there are less than two chracters, return the string:  "Error".
+# Use this link if you need help/hint.
+# (https://stackoverflow.com/questions/7983820/get-the-last-4-characters-of-a-string)
+
+
+
+def last_two(mystring):
+    if len(mystring)<2:
+        return ('Error')
+    else:
+        return (mystring[-2:])
+
+
+print(last_two('j'))
