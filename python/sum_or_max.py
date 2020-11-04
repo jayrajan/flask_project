17b6b9376756543352beee58defde43c8a5749ff
Jerin Rajan <jrajan@jprtech.co.uk>
2020-11-04 18:58:22 +0000

    initial commit

---
+++ python/sum_or_max.py
@@ -0,0 +1,12 @@
+# ## Task 7
+#
+# Given a list of integers, if the length of the list is an even number,
+# return the sum of the list. If the length of the list is odd, return the max
+## value in that list.
+
+
+
+def sum_or_max(mylist):
+
+    # Code Here
+    pass
