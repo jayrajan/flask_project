7b7bc69eef7db5f8b19651edc7095e66d9d557dd
Jerin Rajan <jrajan@jprtech.co.uk>
2020-11-04 18:49:21 +0000

    initial commit

---
+++ python/compare_len.py
@@ -0,0 +1,11 @@
+# ## Task 6
+#
+# Given a 2 strings, create a function that returns the difference in length
+# between them. This difference in length should always be a positive number
+# (or just 0). Hint: Absolute Value.**
+
+
+
+def compare_len(s1,s2):
+    # Code Here
+    pass
