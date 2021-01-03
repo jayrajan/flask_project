6338d6b1915252bab2422ad49024560f73e0ad8e
Jerin Rajan <jrajan@jprtech.co.uk>
2021-01-03 15:44:43 +0000

    feat: instnce of web app, decorators, defining function views for home page, calling app()

---
+++ flask/myflask.py
@@ -0,0 +1,17 @@
+# creating a simple website using Flask
+
+from flask import Flask
+
+# create an instance of Flask class
+app = Flask(__name__)
+
+# Decorators - defining weblinks
+@app.route('/')
+# view to be returned when home page is called
+def home:
+    return '<h1> Welcome to Jays page </h1>'
+
+if __name__ == "__main__":
+    app.run()
+
+    
