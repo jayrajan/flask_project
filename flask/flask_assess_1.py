33d072c1237c02c62606472405d8ddba38dfba74
Jerin Rajan <jrajan@jprtech.co.uk>
2021-01-12 15:14:31 +0000

    feat: app instance, defining routes, func-puppy_latin

---
+++ flask/flask_assess_1.py
@@ -0,0 +1,20 @@
+# Code written by Jerin Rajan on 12th Jan 2020
+
+from flask import Flask
+
+# creating an instance of Flask application
+app = Flask(__name__)
+
+# Decorators
+@app.route('/')
+def index:
+    return ('<h1> Welcome! Go to puppy_latin/name to see name in puppy latin </h1>')
+
+@app.route('/puppy_latin/<name>')
+def puppy_latin(name):
+    latin_name = name
+    return ('<h2> Hi <name> ! Your puppy latin name is <latin_name> </h2>')
+
+if __name__='__main__':
+    app.run()
+
