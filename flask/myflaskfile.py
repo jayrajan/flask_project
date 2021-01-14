f9b5584d8a0d15a7de28aace3a76a6f26e79fcf3
Jerin Rajan <jrajan@jprtech.co.uk>
2021-01-14 22:04:54 +0000

    feat: user_logged_in variable

--- flask/myflaskfile.py
+++ flask/myflaskfile.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     name = 'Jay'
-    return render_template('index.html', name=name)
+    user_logged_in = False
+    return render_template('index.html', name=name, user_logged_in=user_logged_in)
 
 if __name__ == "__main__":
     app.run(debug=True)
