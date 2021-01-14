947658395e8485592cf3dbe1281932fa26b8c7ed
Jerin Rajan <jrajan@jprtech.co.uk>
2021-01-14 17:37:09 +0000

    feat: linked index.html view with func - home

--- flask/myflaskfile.py
+++ flask/myflaskfile.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('')
+    return render_template('index.html')
 
 if if __name__ == "__main__":
     app.run(debug=True)
-    
