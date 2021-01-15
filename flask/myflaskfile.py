fc1317b500e1d28336a4ca9eab66cdf47ddc2dae
Jerin Rajan <jrajan@jprtech.co.uk>
2021-01-15 22:43:14 +0000

    fix: name variable

--- flask/myflaskfile.py
+++ flask/myflaskfile.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    name = 'Jay'
+    name = 'Jay Rajan'
     user_logged_in = False
     return render_template('index.html', name=name, user_logged_in=user_logged_in)
 
